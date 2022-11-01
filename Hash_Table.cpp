bce99eafc9b8e2cf06b571353de972339612579a
G Srinivasa Eswara Vara Prasad <105410227+varaprasadtarunkumar@users.noreply.github.com>
2022-11-01 20:51:03 +0530

    Add files via upload

---
+++ Hash_Table.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+//class Node{
+//	public:
+//	 int data;
+//	 Node* next;
+//};
+// void swap(arr[key],arr[siz])
+int main(){
+	int size,i;
+	cout<<"Enter the size "<<endl;
+	cin>>size;
+	int arr[size],hashtable[size];
+	cout<<"Enter the elements"<<endl;
+	for(i=0;i<size;i++){
+		cin>>arr[i]; 
+		}
+	for(i=0;i<size;i++){
+		int key=arr[i]%size;
+		hashtable[key]=arr[i];
+		//swap(arr[key],arr[i]);
+		/*int temp=arr[key];
+		arr[key]=arr[i];
+		arr[i]=temp; */
+		//arr[i]=arr[key];
+	}
+	cout<<"The array is "<<endl;
+	for(i=0;i<size;i++){
+		cout<</*arr[i] */hashtable[i]<<" ";
+	}
+}
+
